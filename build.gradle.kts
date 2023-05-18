696dbf32d361c20321101fa82220791d345ba4d3
COBaOrNotCOBa <yureal1986@mail.ru>
2023-05-18 16:30:19 +0300

    Start 18.05.2023

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.8.21"
+    kotlin("plugin.serialization") version "1.8.21"
     application
 }
 
@@ -8,10 +9,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation ("com.airtable:airtable-java:1.0.5")
 }
 
 tasks.test {
@@ -19,7 +23,7 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(18)
 }
 
 application {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,57 @@
-fun main() {
-    println("Hello World!")
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import com.airtable.v0.*
+import com.airtable.v0.models.Record
+import com.airtable.v0.services.Airtable
+import kotlinx.serialization.json.Json
+
+//@Serializable
+//data class Update(
+//    @SerialName("update_id")
+//    val updateId: Long,
+//    @SerialName("message")
+//    val message: Message? = null,
+//    @SerialName("callback_query")
+//    val callbackQuery: CallbackQuery? = null,
+)
+
+//@Serializable
+//data class Response(
+//    @SerialName("result")
+//    val result: List<Update>,
+//)
+
+fun main(args: Array<String>) {
+    val airtableBotToken = args[0]
+    var lastUpdateId = 0L
+    val json = Json { ignoreUnknownKeys = true }
+    val airtable = Airtable(airtableBotToken, "app9he8qhzLjlZo56")
+    val table = airtable.table("fldvu0eaKuAwQ6LWO")
+
+    val result = runCatching { getUpdates(airtableBotToken, lastUpdateId) }
+    val responseString = result.getOrNull()
+    println(responseString)
+
+    println(table)
+}
+
+class Airtable(airBotToken: String, tableId: String) {
+    fun table(tableName: String): String {
+    return tableName
+    }
+
+}
+
+fun getUpdatesAirTable(botToken: String, updateId: Long): String {
+//    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
+    val urlGetUpdates = "https://api.airtable.com/v0/app9he8qhzLjlZo56/Place \\" +
+            "-H Authorization: Bearer $botToken"
+    val client: HttpClient = HttpClient.newBuilder().build()
+    val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
+    val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
+    return response.body()
 }
